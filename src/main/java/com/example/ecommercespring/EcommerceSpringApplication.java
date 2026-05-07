3ef73869ffed770fb19682ab7bf0953ac3147ea1
RavRaj Kumar Yadav <ravirajkumar758@gmail.com>
2026-05-07 18:09:46 +0530

    Snderstand about.env file

--- src/main/java/com/example/ecommercespring/EcommerceSpringApplication.java
+++ src/main/java/com/example/ecommercespring/EcommerceSpringApplication.java
@@ -1,5 +1,5 @@
 package com.example.ecommercespring;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +8,20 @@ public class EcommerceSpringApplication {
 
 	public static void main(String[] args) {
 
+		// load environment file from .env file
+		Dotenv dotenv = Dotenv.configure().load();
+
+
+		// Set all .env variables as system properties
+		dotenv.entries().forEach( entry ->
+				System.setProperty(entry.getKey(), entry.getValue()))
+		;
+
 		SpringApplication.run(EcommerceSpringApplication.class, args);
 	}
-
 }
+
+
+
+
+
